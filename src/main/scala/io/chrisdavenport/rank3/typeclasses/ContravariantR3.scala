d5a280c0f936408e9f80bccab9399a0bf4657e99
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-11-02 09:26:12 -0400

    Rename Typeclass Correctly

--- src/main/scala/io/chrisdavenport/rank3/typeclasses/ContravariantR3.scala
+++ src/main/scala/io/chrisdavenport/rank3/typeclasses/ContravariantR3.scala
@@ -3,7 +3,7 @@ package typeclasses
 
 // What in the world operates at Rank4 to utilize a Rank3 Function.
 // Cool concept.
-trait CovariantR3[A[_[_[_]]]] extends InvariantR3[A]{
+trait ContravariantR3[A[_[_[_]]]] extends InvariantR3[A]{
   def contramapR3[F[_[_]], G[_[_]]](fa: A[F])(f: G ~~> F): A[G]
   def imapR3[F[_[_]], G[_[_]]](fa: A[F])(f: F ~~> G)(g: G ~~> F): A[G] = 
     contramapR3(fa)(g)
