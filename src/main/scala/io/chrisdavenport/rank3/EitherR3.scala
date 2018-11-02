3ac8277091437e15f69e5ef01e85213fe069955e
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-11-02 17:14:30 -0400

    Add Elevation Arrows

--- src/main/scala/io/chrisdavenport/rank3/EitherR3.scala
+++ src/main/scala/io/chrisdavenport/rank3/EitherR3.scala
@@ -12,14 +12,22 @@ final case class EitherR3[F[_[_]], G[_[_]], H[_]](run: Either[F[H], G[H]]){
   def leftmapR3[N[_[_]]](f: F ~~> N): EitherR3[N, G, H] =
     semiFold(fh => EitherR3.leftc(f(fh)), EitherR3.rightc(_))
   def bimapR3[M[_[_]], N[_[_]]](f: F ~~> M, g: G ~~> N): EitherR3[M,N, H] = 
-    semiFold(fh => EitherR3.leftc(f(fh)), gh => EitherR3.rightc(g(gh)))
-
+    fold(f.andThenR3(EitherR3.liftLeft[M, N]), g.andThenR3(EitherR3.liftRight[M,N]))
+    
   def semiFold[C](left: F[H] => C, right: G[H] => C): C = run.fold(left(_), right(_))
   def fold[M[_[_]]](left: F ~~> M, right: G ~~> M): M[H] = run.fold(left(_), right(_))
 }
 object EitherR3 {
   def leftc[F[_[_]], G[_[_]], H[_]](left: F[H]): EitherR3[F, G, H] = EitherR3(Left(left))
   def rightc[F[_[_]], G[_[_]], H[_]](right: G[H]): EitherR3[F, G, H] = EitherR3(Right(right))
+  def liftLeft[F[_[_]], G[_[_]]]: F ~~>  EitherR3[F, G, ?[_]] = new Function1R3[F, EitherR3[F, G, ?[_]]]{
+    def apply[H[_]](fh: F[H]): EitherR3[F, G, H] =
+      leftc(fh)
+  }
+  def liftRight[F[_[_]], G[_[_]]]: G ~~> EitherR3[F, G, ?[_]] = new Function1R3[G, EitherR3[F, G, ?[_]]]{
+    def apply[H[_]](gh: G[H]): EitherR3[F, G, H] = 
+      rightc(gh)
+  }
 
   // Could just a easily be a Bifunctor
   implicit def functorEitherR3[F[_[_]], H[_]] = {
