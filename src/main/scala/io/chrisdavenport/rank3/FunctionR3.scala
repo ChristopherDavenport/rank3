7cd1adea530425b1d41e73af11f26ca5b5f26925
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-11-02 16:55:30 -0400

    Add Compose and andThen Rank3 Combinators

--- src/main/scala/io/chrisdavenport/rank3/FunctionR3.scala
+++ src/main/scala/io/chrisdavenport/rank3/FunctionR3.scala
@@ -2,6 +2,21 @@ package io.chrisdavenport.rank3
 
 trait Function1R3[A[_[_]], Y[_[_]]]{
   def apply[Z[_]](a: A[Z]): Y[Z]
+  def composeR3[M[_[_]]](f: M ~~> A): Function1R3[M, Y] = Function1R3.composeR3(this)(f)
+  def andThenR3[M[_[_]]](f: Y ~~> M): Function1R3[A, M] = Function1R3.andThenR3(this)(f)
+}
+object Function1R3 {
+  def composeR3[B[_[_]],C[_[_]], A[_[_]]](init: B ~~> C)(retreat: A ~~> B): A ~~> C = 
+    new Function1R3[A, C]{
+      def apply[Z[_]](a: A[Z]): C[Z] = 
+        init(retreat(a))
+    }
+  def andThenR3[A[_[_]], B[_[_]], C[_[_]]](init: A ~~> B)(advance: B ~~> C): A ~~> C =
+    new Function1R3[A, C]{
+      def apply[Z[_]](a: A[Z]): C[Z] = 
+        advance(init(a))
+    }
+
 }
 trait Function2R3[A[_[_]],B[_[_]],Y[_[_]]]{
   def apply[Z[_]](a: A[Z], b: B[Z]): Y[Z]
