3a1756e56df354c6b00d9575f17ecdafda549264
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-11-02 09:05:30 -0400

    Add Compilation of Function Building

--- src/test/scala/io/chrisdavenport/rank3/CompileSpec.scala
+++ src/test/scala/io/chrisdavenport/rank3/CompileSpec.scala
@@ -82,6 +82,45 @@ object CompileSpec extends mutable.Specification {
 
     val test = Tuple2R3(Monad[Option], Tuple2R3(foo, bar))
     val _ = transform(test)
+    
+  }
+  val function = {
+        trait Foo[F[_]]{
+      def foo: F[Int]
+    }
+    trait Bar[F[_]]{
+      def bar: F[Int]
+    }
+
+    val functionR3 = new ~~>[Foo, Bar]{
+      def apply[F[_]](fh: Foo[F]): Bar[F] = new Bar[F]{
+        def bar = fh.foo
+      }
+    }
+
+    trait Baz[F[_]]{
+      def baz: F[(Int, Int)]
+    }
+
+    val foo = new Foo[Option]{
+      def foo: Option[Int] = Some(1)
+    }
+    val bar = functionR3(foo)
+
+
+    import cats._
+    import cats.implicits._
+
+    val function3 = new Function3R3[Monad, Foo, Bar, Baz]{
+      def apply[F[_]](m: Monad[F], foo: Foo[F], bar: Bar[F]): Baz[F] = new Baz[F] {
+        implicit val M = m
+        def baz = for {
+          foo <- foo.foo
+          bar <- bar.bar
+        } yield (foo, bar)
+      }
+    }
+    val _ = function3(Monad[Option], foo, bar)
   }
 
 }
