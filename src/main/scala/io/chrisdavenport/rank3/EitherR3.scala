a62a0e2d79b12f23ea0d94099a48362c476a6d23
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-11-02 12:00:56 -0400

    Proof of Concept Rank3 Functor Implementation

--- src/main/scala/io/chrisdavenport/rank3/EitherR3.scala
+++ src/main/scala/io/chrisdavenport/rank3/EitherR3.scala
@@ -1,6 +1,7 @@
 package io.chrisdavenport.rank3
 
 import cats.implicits._
+import typeclasses.FunctorR3
 
 final case class EitherR3[F[_[_]], G[_[_]], H[_]](run: Either[F[H], G[H]]){
   def left: Option[F[H]] = run.fold(_.some, _ => None)
@@ -12,4 +13,13 @@ final case class EitherR3[F[_[_]], G[_[_]], H[_]](run: Either[F[H], G[H]]){
 object EitherR3 {
   def leftc[F[_[_]], G[_[_]], H[_]](left: F[H]): EitherR3[F, G, H] = EitherR3(Left(left))
   def rightc[F[_[_]], G[_[_]], H[_]](right: G[H]): EitherR3[F, G, H] = EitherR3(Right(right))
+
+  // Could just a easily be a Bifunctor
+  implicit def functorEitherR3[F[_[_]], H[_]] = {
+    type PartiallyApliedEitherR3[G[_[_]]] = EitherR3[F, G, H]
+    new FunctorR3[PartiallyApliedEitherR3]{
+      def mapR3[M[_[_]], N[_[_]]](fa: EitherR3[F, M, H])(f: M ~~> N): EitherR3[F, N, H] = 
+        fa.semiFold(fh => leftc(fh), mh => rightc(f(mh)))
+    }
+  }
 }
--- src/main/scala/io/chrisdavenport/rank3/package.scala
+++ src/main/scala/io/chrisdavenport/rank3/package.scala
@@ -2,4 +2,7 @@ package io.chrisdavenport
 
 package object rank3 {
   type ~~>[F[_[_]], G[_[_]]] = Function1R3[F, G]
+  def idR3[F[_[_]]] = new ~~>[F, F]{
+    def apply[H[_]](fa: F[H]): F[H] = fa
+  }
 }
